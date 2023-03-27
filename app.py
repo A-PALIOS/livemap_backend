09812be960ac8808861ae4b4d6f4f49dccd3baa4
A-PALIOS <paliosadonis6@gmail.com>
2023-03-27 16:49:08 +0300

    Antonis Commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import shapely.geometry
 import folium
 import requests
 
-APP_TITLE = 'Κ.Ο.Μ.Υ. backend 1.0'
+APP_TITLE = 'Κ.Ο.Μ.Υ. backend 1.1'
 APP_SUB_TITLE = 'by CMT Prooptiki'
 def geojson_maker(file1,file2):
     #map_data3 = pd.read_excel('komgeodata_despina.xlsx',dtype={'KALCODE':str})
